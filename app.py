08e24af336abd0b3fc489164474d26594852a51c
jay-1799 <77386447+jay-1799@users.noreply.github.com>
2021-01-18 16:40:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+from tensorflow.keras.models import Sequential, load_model
+from keras.preprocessing import image
+import tensorflow as tf
+global graph
+graph = tf.compat.v1.get_default_graph()
+from flask import Flask , request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+#from igraph import Graph
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/predict',methods = ['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['image']
+        print("current path")
+        basepath = os.path.dirname(__file__)
+        print("current path", basepath)
+        filepath = os.path.join(basepath,'uploads',f.filename)
+        print("upload folder is ", filepath)
+        f.save(filepath)
+        
+        img = image.load_img(filepath,target_size = (64,64))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x,axis =0)
+        
+        with graph.as_default():
+            model = load_model("xray.h5")
+            p = model.predict_classes(x)
+            print(p)
+        
+        index = ["NORMAL","PNEUMONIA"]
+        #text1 = pred[0] 
+        text = str(index[p[0]])
+            #predicted_class = 'pneumonia' if preds[0] > 0.5 else 'normal'
+
+    return text
+            #'pneumonia_probability': str(preds[0])}
+if __name__ == '__main__':
+    app.run(debug = True, threaded = False)
